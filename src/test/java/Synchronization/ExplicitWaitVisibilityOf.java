badde99563ebfe8bbf7b058ae72096b223722fe9
Harikaaaaaaa <143239806+Harikaaaaaaa@users.noreply.github.com>
2023-08-28 09:25:41 +0530

    Update ExplicitWaitVisibilityOf.java

--- src/test/java/Synchronization/ExplicitWaitVisibilityOf.java
+++ src/test/java/Synchronization/ExplicitWaitVisibilityOf.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//This is an example for explicitlywait operation
 public class ExplicitWaitVisibilityOf 
 {
 	public static void main(String[] args)
